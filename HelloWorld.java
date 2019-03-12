bf283ce479a0eadbea30a975ee5dff0535631c08
hydra3230 <martin3230@foxmail.com>
2019-03-12 17:03:39 +0800

    Create
    The  creating of the Helloworld.

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test1;
+
+/**
+ * @author bsnpc5i
+ *
+ */
+public class test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
